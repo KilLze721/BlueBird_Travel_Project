5b9bf03c8a344862c26bee1323fed6e5ec4eae44
KilLze <kirito24177@163.com>
2025-12-10 20:27:11 +0800

    修改xml映射文件位置

--- src/main/java/com/bird/bluebirdproject/BlueBirdProjectApplication.java
+++ src/main/java/com/bird/bluebirdproject/BlueBirdProjectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
-@MapperScan("com.bird.bluebirdproject.mapper")
 public class BlueBirdProjectApplication {
 
     public static void main(String[] args) {
